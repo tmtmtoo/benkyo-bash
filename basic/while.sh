734e28c5c63d556093eb744b56c9aca72ff1c816
tmtmtoo <tmtmt2209@gmail.com>
2022-12-29 12:13:29 +0900

    while statement

---
+++ basic/while.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+TIMEOUT_SEC=$1
+: ${MAX:=5}
+
+COMMAND=$2
+: ${COMMAND:='echo enter the command.'}
+
+BASE_TIME=$(date +%s)
+
+while ! eval $COMMAND; do
+	NOW=$(date +%s)
+	DIFF=$((NOW - BASE_TIME))
+
+	[ $DIFF -gt $TIMEOUT_SEC ] &&
+		echo 'timeouted' &&
+		break
+done
+
+echo 'done'
+
